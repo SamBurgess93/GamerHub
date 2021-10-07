c2d8432598e4d4ac9dd52c354d6c441f64e4ab6c
SamBurgess93 <samburgess456@gmail.com>
2021-10-07 09:37:46 +0000

    Added input fields to register.html

--- app.py
+++ app.py
@@ -28,6 +28,11 @@ def get_games():
     )
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
